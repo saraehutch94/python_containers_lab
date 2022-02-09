1d74d0e8a3b2334bd195adf3f35793231591984d
Sara Hutchinson <84148986+saraehutch94@users.noreply.github.com>
2022-02-09 02:40:17 +0000

    exercise 2 / foods tuple

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ print(students[1])
 
 # last student
 print(students[-1])
+
+# Exercise 2:
+
+# foods tuple
+foods = ('pizza', 'pasta', 'tostones', 'empanadas', 'gelato', 'sorbet')
+
+# for loop --> iterate over foods tuple
+for food in foods:
+  print(f'{food} is a good foods')
