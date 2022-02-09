233d53c96e81c42c7036e0e31d5d77e0e9f8b57f
Sara Hutchinson <84148986+saraehutch94@users.noreply.github.com>
2022-02-09 02:37:46 +0000

    exercise 1 / students list

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('hello world')
+# Exercise 1:
+
+# students list
+students = ['Sara', 'Johaisi', 'Delimar', 'Javier', 'Ataur', 'Phil']
+
+# second student
+print(students[1])
+
+# last student
+print(students[-1])
